f97cb0f21982e9481758e45f38e473ac954bc04d
Litexmary <102751808+Litexmary@users.noreply.github.com>
2026-09-13 20:28:44 +0300

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let myCourse = ["learn css animation", "ui design fundamentals", "intro to clean code"]
+function courses(arr){
+  for(let i = 0; i < arr.length; i++){
+  console.log(arr[i]);
+  }
+}
+
+courses(myCourse)
